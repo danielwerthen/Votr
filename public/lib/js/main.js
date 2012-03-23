5bfc3586b00e8274f1741a7a7fe7b61ee16a5e83
Daniel Werthén <danielwerthen@gmail.com>
2012-03-23 10:13:16 +0100

    Works?

--- public/lib/js/main.js
+++ public/lib/js/main.js
@@ -7,9 +7,22 @@ require(
 		$(function () {
 			var port = $('body').data('port');
 			var socket = io.connect('http://localhost:' + port);
-			socket.on('news', function (data) {
-				console.log(data);
-			});
+
+			var answer = $('#answer');
+			if (answer.length > 0) {
+				answer.find('.btn').click(function () {
+					var btn = $(this)
+						, id = answer.data('id');
+					$.post('/answer/' + id, { answer: btn.attr('id') }, function (data) {
+						answer.empty();
+						answer.load('/answers/' + id);
+						socket.emit('listen', { id: id });
+						socket.on('update', function (data) {
+							answer.load('/answers/' + id);
+						});
+					});
+				});
+			}
 
 
 		});
--- server.js
+++ server.js
@@ -21,22 +21,45 @@ app.post('/question', function (req, res) {
 		, yesno = req.body.yesno
 		, id = questions.length
 
-	questions.push({ question: question, answers: [], yesno: yesno !== undefined });
+	questions.push({ question: question, answers: [], yesno: yesno !== undefined , id: id });
 
 	res.redirect('/question/' + id);
 });
 
+app.post('/answer/:id', function (req, res) {
+	if (req.params.id >= 0 && req.params.id < questions.length) {
+		var q = questions[req.params.id];
+		q.answers.push(req.body.answer === 'yes');
+		if (q.sockets) {
+			for (var i in q.sockets) {
+				q.sockets[i].emit('update', { id: req.params.id });
+			}
+		}
+		res.end('ok');
+	}
+	else return res.end('error');
+});
+
+app.get('/answers/:id', function (req, res) {
+	if (req.params.id >= 0 && req.params.id < questions.length) {
+		res.partial('answers', { q: questions[req.params.id] });
+	}
+	else return res.end('error');
+});
+
 app.get('/question/:id', function (req, res) {
 	if (req.params.id >= 0 && req.params.id < questions.length) {
-		res.render('question', questions[req.params.id]);
+		res.render('question', { q: questions[req.params.id] });
 	}
 	else return res.end('error');
 });
 
 io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
+	socket.on('listen', function (data) {
+		if (data.id >= 0 && data.id <= questions.length) {
+			questions[data.id].sockets = questions[data.id].sockets || [];
+			questions[data.id].sockets.push(socket);
+		}
 	});
 });
 
